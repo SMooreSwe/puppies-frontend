1614e817cff85fccaef6078ce54e568b2d8d6637
SMooreSwe <stevedm88@gmail.com>
2023-06-09 10:15:21 +0200

    added bottom margin to title

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
     <>
       <img src="/sleepy.jpg" className="opacity-20 absolute left-0 top-0 w-[100%] h-[100%] -z-50 object-cover blur-sm" alt="background puppy" />
       <main className="flex min-h-screen flex-col items-center justify-center gap-2 px-10 lg:px-4 py-2">
-        <h1 className='text-2xl md:text-6xl font-extrabold text-blue-500'>Pick a Puppy</h1>
+        <h1 className='text-2xl md:text-6xl font-extrabold text-blue-500 mb-6'>Pick a Puppy</h1>
         <section className='flex flex-col gap-4 items-center'>
         <div className="scroller snap-x snap-mandatory grid gap-6 px-6 pb-4 mt-1 max-w-[90vw] grid-flow-col overflow-x-auto overscroll-contain auto-cols-[27%] sm:auto-cols-[80px] xl:auto-cols-[160px]">
           {puppyList && puppyList.map(puppy => {
