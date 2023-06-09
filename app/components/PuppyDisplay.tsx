efe445be5da27d7d0d6a745e3312dac6094cb699
SMooreSwe <stevedm88@gmail.com>
2023-06-09 09:59:56 +0200

    tweaked mobile display

--- app/components/PuppyDisplay.tsx
+++ app/components/PuppyDisplay.tsx
@@ -26,7 +26,7 @@ const PuppyDisplay = (props: PuppyDisplayProps) => {
   }
 
   return (
-    <motion.section className='flex flex-col justify-end bg-sky-400 text-white px-12 pb-4 pt-4 h-[60svh] md:h-[50svh] rounded-lg shadow-lg'
+    <motion.section className='flex flex-col justify-end bg-sky-400 text-white px-12 pb-4 pt-4 h-[50svh] md:h-[45svh] rounded-lg shadow-lg'
     variants={container}
     initial="start"
     animate="end">
